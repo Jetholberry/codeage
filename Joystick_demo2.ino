e43230077673cc114a177d08a37b5b02d24eb21f
Jetholberry <63824588+Jetholberry@users.noreply.github.com>
2020-05-02 01:11:56 +0100

    Add files via upload

---
+++ Joystick_demo2.ino
@@ -0,0 +1,35 @@
+int vrx = A0;
+int vry = A1;
+int led = 12;
+int xPosition;
+int yPosition;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(vrx, INPUT);
+  pinMode(vry, INPUT);
+  digitalWrite(led, OUTPUT);
+  digitalWrite(led, LOW);
+
+}
+
+void loop() {
+  xPosition = analogRead(vrx);
+  yPosition = analogRead(vry);
+  delay(200);
+  Serial.print("X: ");
+  Serial.println(xPosition);
+  Serial.print("Y: ");
+  Serial.println(yPosition);
+  Serial.println(". . .");
+
+  if (analogRead(xPosition == 0);
+      digitalWrite(led, HIGH));
+
+} else {
+  digitalWrite(led, LOW);
+}
+delay(1000);
+
+
+}
